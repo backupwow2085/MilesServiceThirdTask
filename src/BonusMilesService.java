dacd571c3d95fbfd05a41a2f70eed9cc6c3f9547
Noren <norenowow@gmail.com>
2025-03-30 21:35:12 +0300

    second

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
     public int calculate(int price) {
-        int rublesPerMile = 20; // 1 миля за каждые 20 рублей
-        return price / rublesPerMile; // Возвращаем количество миль
+        int rublesPerMile = 20;
+        return price / rublesPerMile;
     }
 }
